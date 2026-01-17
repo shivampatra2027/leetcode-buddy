dd87cf774f4f3c92423ff4ced0c18c214cf07010
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2026-01-17 23:43:57 +0530

    remove unnecessary

--- extension/src/entrypoints/background.ts
+++ extension/src/entrypoints/background.ts
@@ -1,7 +1,6 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
 
-  // Open sidepanel when extension icon is clicked
   browser.action.onClicked.addListener(async (tab) => {
     if (tab.id && tab.windowId) {
       await browser.sidePanel.open({ windowId: tab.windowId });
